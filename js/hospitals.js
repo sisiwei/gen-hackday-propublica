49952fa1d43550a63e57d8828dc55d0f7e1408b1
Sisi Wei <me@sisiwei.com>
2013-04-21 09:48:56 -0400

    I don't even know

--- js/hospitals.js
+++ js/hospitals.js
@@ -1,12 +1,9 @@
-var NEEDSHELP = 0;
+var TOTAL = 0,
+	NEEDSHELP = 0;
 var map;
 
 $(document).ready(function(){
 	loadMaps();
-	travelTime("Brooklyn", "Sunnyside, Queens");
-	// $('#hospitalized').html(generatePeople(40, "red"));
-	// $('#survived').html(generatePeople(2, "green"));
-	// $('#deceased').html(generatePeople(10, "red"));
 
 	// Handling the opening screen
 	$('.start').click(function(){
@@ -15,7 +12,6 @@ $(document).ready(function(){
 		generateVictim();
 		window.setInterval(function() { generateVictim() }, 3000 );
 	})
-
 
 })
 
